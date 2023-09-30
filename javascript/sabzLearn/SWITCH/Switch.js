1e3a75f181ceb723426fd9cbcffa8d6a30740910
Amirhossein-Mirzaee <amir2021zx@gmail.com>
2023-09-30 11:51:14 +0330

    switch in sabz learn

---
+++ javascript/sabzLearn/SWITCH/Switch.js
@@ -0,0 +1,35 @@
+var average = prompt("Entert your average??");
+
+switch (average) {
+  case "20":
+  case "19":
+  case "18":
+  case "17":
+  case "16":
+  case "15":
+    alert("بابا به به");
+    break;
+  case "14":
+  case "13":
+  case "12":
+  case "11":
+  case "10":
+    alert("ریدی");
+    break;
+
+  case "9":
+  case "8":
+  case "7":
+  case "6":
+  case "5":
+  case "4":
+  case "3":
+  case "2":
+  case "1":
+  case "0":
+    alert("کیر توت");
+    break;
+  default:
+    alert("خالکصده نمره بین صفر تا بیسته");
+    break;
+}
