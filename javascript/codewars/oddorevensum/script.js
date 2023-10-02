04fc97f7a196aa421b0a197023ef8a2e245db1cb
Amirhossein-Mirzaee <amir2021zx@gmail.com>
2023-10-02 00:13:45 -0800

    loops

--- javascript/codewars/oddorevensum/script.js
+++ javascript/codewars/oddorevensum/script.js
@@ -2,23 +2,16 @@ function oddOrEven(arr) {
   return arr.reduce((a, b) => a + b, 0) % 2 ? "odd" : "even";
 }
 
-const array = [7, 4, 9, 6, 5, 2];
-const target = 6;
+const array = [7, 4, 9, 6, 5, 2, 5];
+const target = 9;
 
-// [7,2,4,5]
-
-const result = array.reduce((prev, current,) => {
-  if (prev.includes(current)) {
-    {
-      return prev;
-    }
-  }
-}, []);
-
-console.log({ result });
-
-//
-// function Google(arr, target) {
-//   return arr.reduce((a, b) => a + b, []) === target;
-// }
-// Google([1, 2, 3], 3);
+const initial = [];
+const result = array.reduce((prev, current) => {
+  const peyda = array.find((item) => item + current === target);
+  console.log(peyda);
+  if (peyda) {
+    prev.push( peyda,  current );
+  } if(prev.includes()) 
+  return prev;
+}, initial);
+console.log(result);
