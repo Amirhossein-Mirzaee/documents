c557477276f1dcfafcb4c163c72915f0ba48cbf2
Amirhossein Mirzaee <amir2021zx@gmail.com>
2023-10-08 23:27:46 -0800

    tempConverter

---
+++ javascript/sabzLearn/Midlevel/miniprojects/temConvertor/source/js/script.js
@@ -0,0 +1,42 @@
+﻿const $ = document;
+
+const firstValue = $.querySelector(".C");
+const secValue = $.querySelector(".F");
+const convBtn = $.querySelector(".convertButton");
+const resBtn = $.querySelector(".resetButton");
+const chgBtn = $.querySelector(".changeButton");
+const converter = $.getElementById("converter");
+const result = $.querySelector(".result");
+
+convBtn.addEventListener("click", () => {
+  if (converter.value === "") {
+    result.innerHTML = "Insert Correct Temp";
+    result.style.color = "red";
+  } else {
+    if (firstValue.innerHTML === "°C") {
+      result.innerHTML =
+        "Temp is:" + " " + (converter.value * 1.8 + 32).toFixed(0) + "°F";
+    } else {
+      result.innerHTML =
+        "Temp is:" + " " + ((converter.value - 32) * 0.55).toFixed(0) + "°C";
+    }
+    result.style.color = "black";
+  }
+});
+resBtn.addEventListener("click", () => {
+  result.innerHTML = "";
+  converter.value = "";
+});
+chgBtn.addEventListener("click", () => {
+  if (firstValue.innerHTML === "°C") {
+    firstValue.innerHTML = "°F";
+    secValue.innerHTML = "°C";
+    converter.placeholder = "°F";
+    $.title = "SalzLearn| Js | °F to °C";
+  } else {
+    firstValue.innerHTML = "°C";
+    secValue.innerHTML = "°F";
+    converter.placeholder = "°C";
+    $.title = "SalzLearn| Js | °C to °F";
+  }
+});
